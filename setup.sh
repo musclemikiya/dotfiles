64e29229172091a421e3e53e74d68b2a16c60a47
username <email@mail.jp>
2014-03-28 04:42:48 +0000

    edit setup.sh

--- setup.sh
+++ setup.sh
@@ -1,1 +1,14 @@
-ln -s /app
+# git clone https://github.com/musclemikiya/dotfiles ~/dotfiles/を実行後
+ln -s ~/dot_files/.vimrc ~/.vimrc
+ln -s ~/dot_files/.vim ~/vim
+git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim/
+#NeoBundleInstall
+
+if[-e ~/.bashrc];then
+	rm -f ~/.bashrc
+fi
+if[-e ~/.vimshrc];then
+	rm -f ~/.vimshrc
+fi
+ln -s ~/dot_files/.bashrc ~/.bashrc
+ln -s ~/dot_files/.vimshrc ~/.vimshrc
