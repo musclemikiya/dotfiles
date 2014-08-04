9e89d2c1fe26e70c2ff34d0879cf4129af0fae06
root <root@aa-dev.localdomain>
2014-08-04 03:11:49 +0000

    セットアップshのif分修正

--- setup.sh
+++ setup.sh
@@ -1,14 +1,14 @@
 # git clone https://github.com/musclemikiya/dotfiles ~/dotfiles/を実行後
-ln -s ~/dot_files/.vimrc ~/.vimrc
-ln -s ~/dot_files/.vim ~/.vim
+ln -s ~/dotfiles/.vimrc ~/.vimrc
+ln -s ~/dotfiles/.vim ~/.vim
 git clone https://github.com/Shougo/neobundle.vim ~/.vim/bundle/neobundle.vim/
 #NeoBundleInstall
 
-if[-e ~/.bashrc];then
+if [-e ~/.bashrc];then
 	rm -f ~/.bashrc
 fi
-if[-e ~/.vimshrc];then
+if [-e ~/.vimshrc];then
 	rm -f ~/.vimshrc
 fi
-ln -s ~/dot_files/.bashrc ~/.bashrc
-ln -s ~/dot_files/.vimshrc ~/.vimshrc
+ln -s ~/dotfiles/.bashrc ~/.bashrc
+ln -s ~/dotfiles/.vimshrc ~/.vimshrc
