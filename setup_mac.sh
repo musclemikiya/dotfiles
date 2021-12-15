8b0841f91e784ae850e6289edd2731ded4dd7e05
musclemikiya <musclemikiya@users.noreply.github.com>
2021-12-15 00:11:47 -0800

    Update setup_mac.sh

--- setup_mac.sh
+++ setup_mac.sh
@@ -1,14 +1,11 @@
 #!/bin/sh 
 brew cask install google-chrome 
 brew cask install slack
-brew cask install phpstorm
 brew cask install chatwork
-brew cask install karabiner
 brew cask install karabiner-elements
 brew cask install alfred
 brew cask install macs-fan-control
 brew cask install iterm2
-brew cask install cmd-eikana
 brew cask install google-japanese-im
 brew cask install docker
 brew cask install atom
